6285bd5e56119287f93e47ee80c4bf9ba1ecb1c0
Raul Escabia <raulescabia.work@gmail.com>
2024-06-20 22:41:37 +0200

    added header and easy styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return <header>
+        <span className="header-title">UI Oasis v0.1</span>
+        <nav className="header-nav">
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Components</a></li>
+                <li><a>Advice</a></li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
-function Home(){
-    return (<>
+function Home() {
+  return (
+    <main >
+      <div className="container">
         <h1>Welcome to the Oasis, this is the home page</h1>
-    </>)
+      </div>
+    </main>
+  );
 }
 
 export default Home;
