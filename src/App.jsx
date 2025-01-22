0feaa8b93b01df58ed7f8a7195d183c0ed8ab823
Raul Escabia <raulescabia.work@gmail.com>
2025-01-22 16:10:41 +0100

    better styled home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import { Navigate, Route, Routes, redirect } from "react-router-dom"
-import Header from "./components/Header"
-import Home from "./pages/Home"
-import Components from "./pages/Components"
-import Advice from "./pages/Advice"
-import PageNotFound from "./pages/PageNotFound"
-
+import { Navigate, Route, Routes, redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Components from "./pages/Components";
+import Advice from "./pages/Advice";
+import PageNotFound from "./pages/PageNotFound";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation()
+  console.log(location)
 
   return (
-    <>
-      <Header/>
-    <Routes>
-      <Route path="/" Component={Home}/>
-      <Route path="/components" element={<Navigate to="/components/introduction"/>}/>
-      <Route path="/components/:title" Component={Components}/>
-      <Route path="/advice" element={<Navigate to="/advice/advice_intro"/>}/>
-      <Route path="/advice/:title" Component={Advice}/>
-      <Route path="*" Component={PageNotFound}/>
-    </Routes>
-    </>
-  )
+    <div className={`main-container ${location.pathname === "/" && "home-background"}`}>
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route
+          path="/components"
+          element={<Navigate to="/components/introduction" />}
+        />
+        <Route path="/components/:title" Component={Components} />
+        <Route
+          path="/advice"
+          element={<Navigate to="/advice/advice_intro" />}
+        />
+        <Route path="/advice/:title" Component={Advice} />
+        <Route path="*" Component={PageNotFound} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,40 @@
 import ExternalLink from "../components/ExternalLink";
-import { useNavigate } from "react-router-dom";
+
 
 function Home() {
-  let navigate = useNavigate()
   return (
-    <main>
-      <div className="container">
-        <h1>Welcome to the Oasis,</h1>
+    <main className="home-page">
+      <div className="home-title">
+        <h1>WELCOME TO THE OASIS</h1>
         <p>
           your go-to resource for building beautiful and functional web
           components with ease.{" "}
         </p>
+      </div>
+      <div className="home-bottom">
+        <div className="home-buttons">
+          <button
+            className="button-primary"
+            onClick={() => navigate("/components")}
+          >
+            Check out components!
+          </button>
+          <button
+            className="button-primary"
+            onClick={() => navigate("/advice")}
+          >
+            Check out advices!{" "}
+          </button>
+        </div>
         <p>
-          Inspiration from:
+          Inspiration from: {" "}
           <ExternalLink link="https://nextui.org/docs" display="NextUI" />
+          {" & "}
           <ExternalLink
             link="https://www.styple.dev/docs/overview/introduction"
             display="Styple"
           />
         </p>
-        <button className="button-outline" onClick={() => navigate("/components")}>Check out components!</button>
-        <button className="button-outline" onClick={() => navigate("/advice")}>Check out advices! </button>
       </div>
     </main>
   );
